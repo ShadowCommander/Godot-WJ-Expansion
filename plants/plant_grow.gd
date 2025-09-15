b12425b2048cbc01cd0292f3a7aa860066f80b48
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2025-09-14 23:01:52 -0700

    Growing works

--- plants/plant_grow.gd
+++ plants/plant_grow.gd
@@ -1,4 +1,4 @@
-extends Node
+extends AnimatedSprite3D
 
 @export var plant_resource: PlantResource
 
@@ -16,9 +16,10 @@ func _physics_process(delta: float) -> void:
 		return
 	growth_timer -= delta
 	var value = (growth_timer / plant_resource.maturation_time)
-	var lerped = lerp(0, 3, value)
+	var frame_count = sprite_frames.get_frame_count("default")
+	var lerped = lerp(0, frame_count - 1, value)
 	var life_stage = ceil(lerped)
 	print("weight: %0.2f, lerp: %0.2f, life stage: %d" % [value, lerped, life_stage])
 	if life_stage == 0:
 		harvestable = true
-	
+	frame = life_stage

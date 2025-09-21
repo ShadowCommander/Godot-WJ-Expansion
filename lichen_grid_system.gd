2454a405ceaf4fe24be7421bf881f9ea7a62318c
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2025-09-21 06:06:19 -0700

    Bandaid fix the nulls in data

--- lichen_grid_system.gd
+++ lichen_grid_system.gd
@@ -33,6 +33,8 @@ func add_plant_to_cells(plant: Plant, cells: Array[Vector3i]) -> void:
 			var plant_to_replace: Plant = null
 			var is_other_gatherer: bool = false
 			for p in plants:
+				if p == null or p.is_queued_for_deletion():
+					continue
 				if p.plant_resource.type != PlantResource.PlantType.Gatherer:
 					continue
 				is_other_gatherer = true
@@ -68,11 +70,13 @@ func remove_plant_from_cells(plant: Plant) -> void:
 
 func internal_erase_plant(plant: Plant, cell: Vector3i) -> void:
 	var offset_cell = cell # + plant.cell 
-	plant_to_grid[plant].erase(offset_cell)
-	if plant_to_grid[plant].size() == 0:
+	var cells = plant_to_grid.get(plant)
+	cells.erase(offset_cell)
+	if cells.size() == 0:
 		plant_to_grid.erase(plant)
-	grid_to_plant[offset_cell].erase(plant)
-	if grid_to_plant[offset_cell].size() == 0:
+	var plants = grid_to_plant.get(offset_cell)
+	plants.erase(plant)
+	if plants.size() == 0:
 		grid_to_plant.erase(offset_cell)
 	print("grid_to_plant: ", grid_to_plant.get(offset_cell))
 	var data: Dictionary = grid_data.get(offset_cell)
@@ -87,9 +91,9 @@ func internal_erase_plant(plant: Plant, cell: Vector3i) -> void:
 		data[plant.plant_resource.type] = type_count
 
 func internal_insert_plant(plant: Plant, cell: Vector3i) -> void:
-	var offset_cell = cell # + plant.cell
-	if not plant_to_grid.has(plant):
+	if plant.is_queued_for_deletion():
 		return
+	var offset_cell = cell # + plant.cell
 	var cells = plant_to_grid.get_or_add(plant, [])
 	if cells.has(offset_cell):
 		return
